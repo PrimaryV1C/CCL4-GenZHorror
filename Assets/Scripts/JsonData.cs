6355cce046dbdecdb9bdbad9f202abcfdb2327d9
PrimaryV1C <petrovictorelias@gmail.com>
2024-06-11 12:06:31 +0200

    Added Json Read Functionality

---
+++ Assets/Scripts/JsonData.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+[System.Serializable]
+public class Dialogue
+{
+    public string text;
+}
+public class JsonData
+{
+    public Dialogue dialogue;
+
+    public static JsonData CreateFromJSON()
+    {
+        string path = Path.Combine(Application.absoluteURL, "Assets/Samples/testDialogue.json");
+        if (File.Exists(path))
+        {
+            string jsonString = File.ReadAllText(path);
+            Dialogue dialogue = JsonUtility.FromJson<Dialogue>(jsonString);
+
+            JsonData jsonHandler = new JsonData();
+            jsonHandler.dialogue = dialogue;
+            return jsonHandler;
+        }
+        else
+        {
+            Debug.LogError("File not found at " + path);
+            return null;
+        }
+    }
+}
