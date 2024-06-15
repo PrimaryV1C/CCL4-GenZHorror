5855983b0be908f62d03d81a0257bfa0a93fbecb
hannahhannah20 <cc221009@fhstp.ac.at>
2024-06-15 14:04:50 +0200

    copying Audiomanager file from before

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,18 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.Common;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   
+   public bool isWalking;
+   public GameObject playerPosition;
 
-    // Update is called once per frame
-    void Update()
+   private Vector3 previousPosition;
+  private Vector3 currentPosition;
+
+  private bool footstepsPlaying = false;
+  private float lastFootstepTime = 0.0f;
+  private float footstepInterval = 0.5f;
+
+  private float movementSpeed = 0.8f;
+
+  public void Start() {
+
+    lastFootstepTime = Time.time;
+
+    if (playerPosition != null) {
+
+      previousPosition = playerPosition.gameObject.transform.position;
+
+  }
+
+  }
+
+
+    public void Update()
     {
-        
+        if (playerPosition != null)
+        {
+            currentPosition = playerPosition.transform.position;
+
+            if (currentPosition != previousPosition)
+
+            {
+
+              isWalking = true;
+                if (Time.time - lastFootstepTime > footstepInterval)
+                {
+                    AkSoundEngine.PostEvent("Play_stepping_container", gameObject);
+                    lastFootstepTime = Time.time;
+            
+                }
+            }
+            else
+            {
+              isWalking = false; 
+
+                if (footstepsPlaying)
+                {
+                    AkSoundEngine.PostEvent("Stop_stepping_container", gameObject); // Ensure there's a stop event in Wwise
+                    footstepsPlaying = false;
+                }
+
+                
+            }
+
+            previousPosition = currentPosition;
+        }
     }
 }
