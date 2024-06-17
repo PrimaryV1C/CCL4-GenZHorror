7672acf5aefcfd80bbb4e91cd77fe41d6b3d3719
PrimaryV1C <petrovictorelias@gmail.com>
2024-06-17 11:44:21 +0200

    Clock transform modified

--- Assets/Wristwatch/Scripts/RealClock.cs
+++ Assets/Wristwatch/Scripts/RealClock.cs
@@ -1,45 +1,52 @@
-﻿//This script uses the system time to position the hands of a clock. You may assign as many or few hands as you wish, but I recommend you use all 3!
-//And of course you can use your own watch model, should work with any as long as the hands are
-//all at 12:00 when their rotation is zeroed and their local Z axis is perpendicular to the clock face
-
-using UnityEngine;
-using System.Collections;
+﻿using UnityEngine;
 using System;
 
 public class RealClock : MonoBehaviour {
-	public Transform Hours;
-	public Transform Minutes;
-	public Transform Seconds;
+    public Transform Hours;
+    public Transform Minutes;
+    public Transform Seconds;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		float hour = System.DateTime.Now.Hour;
-		float minute = System.DateTime.Now.Minute;
-		float second = System.DateTime.Now.Second;
-		float millisecond = System.DateTime.Now.Millisecond;
+    private DateTime initialTime;
+    private TimeSpan startTimeOffset;
 
-		//we want these hands to move smoothly, not jump every time the hour/minute changes. 
-		hour = hour + minute / 60f;
-		minute = minute + second / 60f;
+    private float startHour = 11;
+    private float startMinute = 57;
+    private float startSecond = 0;
 
-		//The second hand is fine jumping, but if you want a smooth rotation on that just uncomment this code:  
-		//second = second + millisecond / 1000f;
+    // Use this for initialization
+    void Start () {
+        // Initialize startTimeOffset to ensure the clock starts at 11:57:00
+        startTimeOffset = new TimeSpan((int)startHour, (int)startMinute, (int)startSecond);
 
+        // Store the initial time when the game starts
+        initialTime = DateTime.Now;
+    }
 
-		if(Hours)
-			Hours.localRotation = Quaternion.Euler (0, 0, hour / 12 * 360);
+    // Update is called once per frame
+    void Update () {
+        // Calculate the elapsed time since the game started
+        TimeSpan elapsedTime = DateTime.Now - initialTime;
 
-		if(Minutes)
-			Minutes.localRotation = Quaternion.Euler (0, 0, minute / 60 * 360);
+        // Calculate the current time on the clock
+        TimeSpan currentTime = startTimeOffset + elapsedTime;
 
-		if(Seconds)
-			Seconds.localRotation = Quaternion.Euler (0, 0, second / 60 * 360);
+        // Extract hours, minutes, and seconds from the current time
+        float hour = currentTime.Hours % 12; // Ensure hours stay within 0-11 range
+        float minute = currentTime.Minutes;
+        float second = currentTime.Seconds + (float)currentTime.Milliseconds / 1000;
 
+        // Smooth hour and minute hand movements
+        hour += minute / 60f;
+        minute += second / 60f;
 
-	}
+        // Update the rotations of the clock hands
+        if (Hours)
+            Hours.localRotation = Quaternion.Euler(0, 0, hour / 12 * 360); // Positive direction
+
+        if (Minutes)
+            Minutes.localRotation = Quaternion.Euler(0, 0, minute / 60 * 360); // Positive direction
+
+        if (Seconds)
+            Seconds.localRotation = Quaternion.Euler(0, 0, second / 60 * 360); // Positive direction
+    }
 }
