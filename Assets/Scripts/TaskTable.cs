b73693567193257c185d7e6b3ff48a7526d3fbd9
PrimaryV1C <petrovictorelias@gmail.com>
2024-06-20 09:52:49 +0200

    beer snaps to table

--- Assets/Scripts/TaskTable.cs
+++ Assets/Scripts/TaskTable.cs
@@ -14,17 +14,19 @@ void OnTriggerEnter(Collider other) {
     if (other.gameObject.CompareTag("TaskItem")){
 
 
+        itemDelivered.Invoke();
+
         Rigidbody rb = other.gameObject.GetComponentInParent<Rigidbody>();
             if (rb != null)
             {
                 rb.isKinematic = true;
             }
         
-        other.gameObject.transform.position = snapPosition.position;
-        other.gameObject.transform.rotation = Quaternion.identity;
-        other.gameObject.GetComponent<XRGrabInteractable>().enabled = false;
+        Transform parentTransform = other.gameObject.GetComponentInParent<Transform>();
+        parentTransform.position = snapPosition.position;
+        parentTransform.rotation = Quaternion.identity;
+        other.gameObject.GetComponentInParent<XRGrabInteractable>().enabled = false;
 
-        itemDelivered.Invoke();
         } 
     }
 }
